abf659f003f34eee6ce44d42bf9a52e9ca7f8b60
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2023-05-11 15:28:03 +0200

    Add files via upload

---
+++ select_nps.py
@@ -0,0 +1,13 @@
+# daten einlesen
+
+# datein in ein pandas frame umwandeln
+
+# aus dem pandas frame daten löschen, die wir nicht benötigen
+
+# aus den übrigen die Nomen heraussuchen
+
+# connections zu den Nomen heraussuchen
+
+# roots herausfiltern
+
+# fertig ? :D
