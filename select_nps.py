7d251c1fa2847211b99c7462c8f9380288fe1761
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2023-05-17 21:40:03 +0200

    Parsing Files select_nps.py
    Added part of the code that reads in all the files and in the same step parses them through the command line

--- select_nps.py
+++ select_nps.py
@@ -1,5 +1,19 @@
+import os
+import glob
+
 # daten einlesen
 
+# every .txt-file in the folder ...
+for filename in glob.glob('*.txt'):
+    # ... will be opened and read
+    with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: # open in readonly mode
+        #os.system('cmd /c "Your Command Prompt Command"')
+        output_filename = str(filename[0:-4]) + "_output.txt"
+        command_for_parsing = "type " + filename + " | docker run -i rsennrich/parzu /ParZu/parzu > " + output_filename
+        command = 'cmd /c "' +  command_for_parsing + '"'
+        print(command)
+        os.system(command)
+        
 # datein in ein pandas frame umwandeln
 
 # aus dem pandas frame daten löschen, die wir nicht benötigen
