4c4fcf492e5f2f81562b53a5738e8c5041341f51
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2023-05-24 14:35:24 +0200

    Update select_nps.py

--- select_nps.py
+++ select_nps.py
@@ -6,6 +6,8 @@ import os
 import glob
 import pandas as pd
 
+
+"""
 # read in all the .txt-files
 for filename in glob.glob('*.txt'):
     # one file at a time will be opened and read
@@ -20,39 +22,59 @@ for filename in glob.glob('*.txt'):
 
         # parse the file
         os.system(command)
-
-# list for the selected nps
-selected_nps = list()
+"""
+collected_nps = list()
 
 # create data frames out of the output.txt-files
 for filename in glob.glob('*output.txt'):
+        with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: # open in readonly mode
+            # extract sentences
+            file_input = file.readlines()
+            # generate a sentence
+            single_sentence = list()
+            for line in file_input:
+                if line[0] != "\n":
+                    single_sentence.append(line)
+                elif line[0] == "\n":
+                    sentence_into_elements = list()
+                    for row in single_sentence:
+                        elements = row.split("\t")
+                        sentence_into_elements.append(elements)
 
-    # read in the file
-    with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: # open in readonly mode
-        file_input = file.readlines()
-        # generate a sentence
-        single_sentence = list()
-        for line in file_input:
-            if line[0] != "\n":
-                single_sentence.append(line)
-            elif line[0] == "\n":
-                sentence_into_elements = list()
-                for row in single_sentence:
-                    elements = row.split("\t")
-                    sentence_into_elements.append(elements)
-                
-                # generate dataframe
-                df = pd.DataFrame(sentence_into_elements, columns = ["index", "token", "lexem", "?", "POS-Tag", "grammatical info", "refers to", "role", "dunno1", "dunno2"])
-                # delete columns we do not need
-                df = df.drop(["lexem", "?","grammatical info", "dunno1", "dunno2"], axis = 1)
-                #print(df)
+                    # create dataframe with column names
+                    df = pd.DataFrame(sentence_into_elements, columns = ["index", "token", "lexem", "Wordtype", "POSTag", "grammatical info", "deprel", "role", "dunno1", "dunno2"])
+                    
+                    # delete columns we do not need
+                    df = df.drop(["lexem","grammatical info", "dunno1", "dunno2"], axis = 1)
 
-                
-                # search for the needed POS-tag and their connection and create a pair of them to add to the list of pairs
-                for ind, row in df.iterrows():
-                    # within that sentence search for the nouns of the sentence
-                    if row["POS-Tag"] == "N": #because PPOSAT refers to his, her, our, my, your, and their
-                        pass
+                    # print the upper three rows
+                    print(df.head(30))
+
+                    token_refers_to_dict = dict()
+
+                    # go through the dataframe of the file
+                    for index, row in df.iterrows():
+                        token_refers_to_dict[row["token"]] = row["deprel"]
+
+                    print(token_refers_to_dict)
+
+                    np = list()
+
+                    for index, row in df.iterrows():
+                         if row["Wordtype"] == "N":
+                            index_of_noun = row["index"]
+                            # check if the any word referes to the noun
+                            key_value_pairs = token_refers_to_dict.items()
+                            np.append(row["token"])
+                            for pair in key_value_pairs:
+
+                                if index_of_noun in pair[1]:
+                                    # get the token
+                                    deprel_key = pair[0]
+                                    print(pair, "\n")
+                                    np.append(pair[0])
+                                    
+                    print(np)
                         # continuation
                         
                         # look out for any token that is conntected to the noun(s)
@@ -64,4 +86,3 @@ for filename in glob.glob('*output.txt'):
                         # finished 
                     
                 # clear the list
-                single_sentence = list()
