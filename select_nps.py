9bc6980274a56ddc7af227b0174a7885a83c6863
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2023-07-11 12:36:44 +0200

    Update select_nps.py

--- select_nps.py
+++ select_nps.py
@@ -1,24 +1,21 @@
-# modules to import
 import os
 import glob
 import pandas as pd
-import ast
+import pickle 
 
-# list to store ALL nps of ONE FILE in
-nps_of_file = list()
+# create new folder for the files
+# folder_name = "data_with_selected_nps"
+# folder = os.path.join("c:/Users/imgey/Desktop/UNI_MASTER_RUB/SoSe23/Korpuslinguistische Analysen der Nominalflexion im Deutschen/automatische NP extraction", folder_name)
 
-sentence_counter = 1
-np_counter = 1
 
 # go through the files in the folder
 for filename in glob.glob('*output.txt'):
 
-    # create a new filename for the to be ouputted file 
-    newfilename = filename + str("_exported_nps.txt")
+    # list to store ALL nps of ONE FILE in
+    nps_of_file = list()
 
-    # create file to save output in 
-    with open(newfilename, "w", encoding="UTF-8-sig") as f:
-        pass
+    sentence_counter = 1
+    np_counter = 1
 
     # open the file in read-mode
     with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: 
@@ -29,7 +26,6 @@ for filename in glob.glob('*output.txt'):
         # list to generate a single sentences out of the file
         single_sentence = list()
         
-
         # go through each line/"sentence" of the file
         for line in file_input:
             # if it is not an empty line/"sentence", add it to the 
@@ -44,22 +40,17 @@ for filename in glob.glob('*output.txt'):
                 for row in single_sentence:
                     elements = row.split("\t")
                     sentence_as_elements.append(elements)
-                # print("Das war ein Satz")
                 
                 # create dataframe with column names
                 df = pd.DataFrame(sentence_as_elements, columns = ["ID", "token", "lemma", "wordtype", "POSTag", "features", "head", "deprel", "dunno1", "dunno2"])
                 
                 # delete columns we do not need
                 df = df.drop(["dunno1", "dunno2"], axis = 1)
-
-                # print the upper three rows
-                # print(df)
-
+                
                 # extract the whole token-column to get the whole sentence as tokens
                 sentence_list = df["token"].values.tolist()
                 sentence_as_string = " ".join(sentence_list)
                 number_of_tokens_of_sentence = len(sentence_list)
-                # print(sentence_as_string)
 
                 token_head_list = list()
 
@@ -101,19 +92,15 @@ for filename in glob.glob('*output.txt'):
                         current_np_ids = list()
 
                         # get all the info of the noun 
-                        info_of_noun = df.loc[index, :].values.flatten().tolist()
-                        # print(info_of_noun)
 
                         id_of_noun = info_of_noun[0]
                         head_of_noun_ID = info_of_noun[6]
 
                         # append the nouns ID to the list
                         current_np_ids.append(id_of_noun)
-                        # print(current_np_ids)
 
                         if int(head_of_noun_ID) != 0:
                             current_np_ids.append(head_of_noun_ID)
-                            # print(current_np_ids)
 
                         # in the following:
                         # check if any word refers to the noun as head
@@ -127,29 +114,21 @@ for filename in glob.glob('*output.txt'):
                                 # --> get a list with IDs, can sort them and 
                                 # then create a second list with the filled in token (information)
                                 current_np_ids.append(token_id_list[ind][1])
-
-                        # print(current_np_ids)
+                                
                         current_np_ids.sort()
                         number_of_tokens_of_np = len(current_np_ids)
-                        # print(current_np_ids)
 
                         # create NP with the regarding token information, based on the IDs
                         current_np = list()
 
                         for token_id in current_np_ids:
-                            # print(token_id)
                             index_in_df = int(token_id) - 1
-                            # print(index_in_df)
                             if int(index_in_df) < 0:
                                 token_info = tuple(df.loc[int(token_id), :].values.flatten().tolist())
                             else:
                                 token_info = tuple(df.loc[int(index_in_df), :].values.flatten().tolist())
 
                             current_np.append(token_info)
-
-                        # print("current_np:\n", current_np, "\n")
-
-                        # print("current sentence:\n", sentence_as_string, "\n")
 
                         """
                         create the final form of the single np on form of:
@@ -168,34 +147,40 @@ for filename in glob.glob('*output.txt'):
                         final_form_of_np = [sentence_np_id, filename, sentence_counter, sentence_as_string, root_of_sentence, number_of_tokens_of_np, current_np]
 
                         np_counter += 1
-                        # print("current form of np:\n", final_form_of_np, "\n")
-
                         
-
-                    # print(final_form_of_np)
                         all_nps_of_sentence.append(final_form_of_np)
-                        # print(all_nps_of_sentence, "\n")
-                        # print(nps_of_file)
-                        # print("\n")
 
                 # --> clear the list of the single sentence for the new sentence
-                # print(single_sentence)
-                # print(sentence_list)
                 single_sentence = list()
-                # print(current_np)
 
                 sentence_counter += 1      
-        # print(all_nps_of_sentence)
+
                 nps_of_file.append(all_nps_of_sentence)  
 
                 # print(nps_of_file)
                 nps_of_file.append("\n")
-        # for element in single_sentence:
-        #     print(element)
 
+    filename_2_0 = filename.replace(".txt", "")
 
-    # print the final ist into a new txt-file for the other groups to 
+    newfilename = str(filename_2_0) + str("_exported_nps.txt")
+
+    # print the final list into a new txt-file for the other groups to 
     # process further 
     with open(newfilename, "a", encoding="UTF-8-sig") as f:
         print(nps_of_file, file=f)
 
+    """ The following commented code-block was intended to be used
+    to create a new folder in the directory and save the new files 
+    in there. But I got an error notice whenever I tried it out and
+    did not figure out yet how to debug it."""
+    # new_filename = newfilename.format(folder_name)
+    # file = os.path.join(folder, new_filename)
+    # os.makedirs(folder)
+    # # print the final ist into a new txt-file for the other groups to 
+    # # process further 
+    # with open(new_filename, "a", encoding="UTF-8-sig") as f:
+    #     print(nps_of_file, file=f)
+
+    # same but as a pickle file
+    # with open(newfilename, 'wb') as f:  # open a text file
+    #     pickle.dump(nps_of_file, f) # serialize the list
