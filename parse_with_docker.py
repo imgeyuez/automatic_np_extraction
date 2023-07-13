3125270fbd8fa74c11124a6c8703f759d4858990
Imge Yüzüncüoglu <86414587+imgeyuez@users.noreply.github.com>
2023-07-13 14:53:08 +0200

    Update parse_with_docker.py

--- parse_with_docker.py
+++ parse_with_docker.py
@@ -6,17 +6,27 @@ import pandas as pd
 command for parsing
 type 1023_0001416.txt | docker run -i rsennrich/parzu /ParZu/parzu > 1023_0001416_output.txt
 """
+
+counter = 0
 # read in all the .txt-files
 for filename in glob.glob('*.txt'):
-    # one file at a time will be opened and read
-    with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: # open in readonly mode
+    if "output" in filename:
+        pass
+    else:
+        # one file at a time will be opened and read
+        with open(os.path.join(os.getcwd(), filename), 'r', encoding="UTF-8-sig") as file: # open in readonly mode
 
-        # create new filename
-        output_filename = str(filename[0:-4]) + "_output.txt"
+            # create new filename
+            output_filename = str(filename[0:-4]) + "_output.txt"
 
-        # create the command
-        command_for_parsing = "type " + filename + " | docker run -i rsennrich/parzu /ParZu/parzu > " + output_filename
-        command = 'cmd /c "' +  command_for_parsing + '"'
+            # create the command
+            command_for_parsing = "type " + filename + " | docker run -i rsennrich/parzu /ParZu/parzu > " + output_filename
+            command = 'cmd /c "' +  command_for_parsing + '"'
 
-        # parse the file
-        os.system(command)
+            # parse the file
+            os.system(command)
+
+        counter += 1
+        print(counter)
+# add, that the files are saved in another folder
+print("finished")
